f1c156229649009ce3e681d7136cfe81f37bcbd6
Josh <joshmemel@gmail.com>
2020-01-05 21:06:30 +1100

    Initial commit

---
+++ precache-manifest.3fb00a11e4b8ca67a27406d80c644d5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31eb277332a7ad480d2130bc7438600b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "440378d35e52d2e2f981",
+    "url": "/static/js/2.e55b7609.chunk.js"
+  },
+  {
+    "revision": "921d57ef98e7aff74fdfbd6ceb0b82d9",
+    "url": "/static/js/2.e55b7609.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5664845c2d39193af066",
+    "url": "/static/js/main.6f699c06.chunk.js"
+  },
+  {
+    "revision": "341539607e70f2d5901a",
+    "url": "/static/js/runtime-main.1ea56d63.js"
+  }
+]);
