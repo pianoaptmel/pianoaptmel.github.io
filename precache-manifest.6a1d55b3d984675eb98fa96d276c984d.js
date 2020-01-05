785919d86cfa63b9f1f26dffa09e25815a23812e
Josh <joshmemel@gmail.com>
2020-01-05 21:14:15 +1100

    Revert "noneed"
    This reverts commit 7325a4bcd45e992aaeb8d8a52a5027539296522c.

---
+++ precache-manifest.6a1d55b3d984675eb98fa96d276c984d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c86543feb385a8e2972b551437535d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "440378d35e52d2e2f981",
+    "url": "/static/js/2.e55b7609.chunk.js"
+  },
+  {
+    "revision": "921d57ef98e7aff74fdfbd6ceb0b82d9",
+    "url": "/static/js/2.e55b7609.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5664845c2d39193af066",
+    "url": "/static/js/main.6f699c06.chunk.js"
+  },
+  {
+    "revision": "341539607e70f2d5901a",
+    "url": "/static/js/runtime-main.1ea56d63.js"
+  }
+]);
